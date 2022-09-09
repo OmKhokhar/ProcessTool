2020580b71c4ef0109236830d1b1e48f6c4b5bd3
Om Khokhar <s3886577@student.rmit.edu.au>
2022-09-09 14:21:02 +1000

    second push

---
+++ src/main/java/com/om/process/Process_Controller.java
@@ -0,0 +1,13 @@
+package com.om.process;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Process_Controller {
+
+    @RequestMapping
+    public String rest(){
+        return "hgukydj";
+    }
+}
