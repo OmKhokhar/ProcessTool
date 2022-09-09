5295096c95390d14bf3c95a9cdfc157af579c633
Om Khokhar <s3886577@student.rmit.edu.au>
2022-09-09 15:38:07 +1000

    final changes

--- src/main/java/com/om/process/model/ItemList.java
+++ src/main/java/com/om/process/model/ItemList.java
@@ -5,16 +5,16 @@ import java.util.List;
 
 public class ItemList
 {
-    private List<com.om.rest.model.Item> ItemLists;
+    private List<com.om.process.model.Item> ItemLists;
 
-    public List<com.om.rest.model.Item> getItemLists() {
+    public List<com.om.process.model.Item> getItemLists() {
         if(ItemLists == null) {
             ItemLists = new ArrayList<>();
         }
         return ItemLists;
     }
 
-    public void setItemLists(List<com.om.rest.model.Item> ItemLists) {
+    public void setItemLists(List<com.om.process.model.Item> ItemLists) {
         this.ItemLists = ItemLists;
     }
 }
